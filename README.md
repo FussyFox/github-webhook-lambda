9b6d8ce9aa886ed666440e10736d875baf532b6d
Johannes Hoppe <info@johanneshoppe.com>
2017-03-01 18:21:50 +0100

    Add chalice app that consumes github hooks and publishes them to SNS

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# github-webhook-lambda
+# Github Webhook receiver for AWS Lambda
+
 Python based AWS lambda function that can receive GitHub webhooks.
+
+### Installation
+
+Make sure you have the AWS cli tools setup. And clone this repository.
+
+```shell
+virtuelenv env -p `which python2`
+pip install -U chalice -r requirements.txt
+SECRET= chalice deploy
+```
+
+Next you need to got to you AWS console and set the Webhooks secret in the
+Lambda environment as `SECRET`.
+
+### Usage
+
+This webhook receiver supports all GitHub hooks.
+You can decide which once you want sent to your Lambda function.
+
+All hooks will be published to SNS in a separate topic for each hook type.
+
+You can write your own little lambda functions and subscribe them to these SNS topics.
---
+++ app.py
@@ -0,0 +1,54 @@
+import hashlib
+import hmac
+import json
+import os
+
+import boto3
+from chalice import BadRequestError, Chalice, UnauthorizedError
+
+DEBUG = os.environ.get('DEBUG', '') in [1, '1', 'True', 'true']
+SECRET = os.environ.get('SECRET')
+
+app = Chalice(app_name='github-webhooks')
+app.debug = DEBUG
+
+SNS = boto3.client('sns')
+
+
+def validate_signature(request):
+    """Validate that the signature in the header matches the payload."""
+    if SECRET is None:
+        return
+    try:
+        signature = request.headers['X-Hub-Signature']
+        _, sha1 = signature.split('=')
+    except KeyError:
+        raise BadRequestError()
+    digest = hmac.new(SECRET, request.raw_body, hashlib.sha1).hexdigest()
+    if not hmac.compare_digest(digest, sha1.encode('utf-8')):
+        raise UnauthorizedError()
+
+
+@app.route('/', methods=['POST'])
+def index():
+    """Consume GitHub webhook and publish hooks to AWS SNS."""
+    request = app.current_request
+    validate_signature(request)
+
+    try:
+        event = request.headers['X-GitHub-Event']
+    except KeyError:
+        raise BadRequestError()
+
+    sns_topics = SNS.list_topics()['Topics']
+    topic_arns = {t['TopicArn'].rsplit(':')[-1]: t['TopicArn'] for t in sns_topics}
+    if event not in topic_arns.keys():
+        topic_arns[event] = SNS.create_topic(Name=event)['TopicArn']
+
+    SNS.publish(
+        TargetArn=topic_arns[event],
+        Message=json.dumps({'default': json.dumps(request.json_body)}),
+        MessageStructure='json'
+    )
+
+    return {'Code': 'Ok', 'Message': 'Webhook received.'}
