cb547d4f8bb18deba53407c71403b063bb778c24
Johannes Hoppe <info@johanneshoppe.com>
2019-12-12 11:39:54 +0100

    Apply some code linters

--- app.py
+++ app.py
@@ -13,72 +13,73 @@ import os
 import boto3
 from chalice import BadRequestError, Chalice, UnauthorizedError
 
-DEFAULT_HASHLIB_BLACKLIST = 'CRC32,CRC32C,MD4,MD5,MDC2'
+DEFAULT_HASHLIB_BLACKLIST = "CRC32,CRC32C,MD4,MD5,MDC2"
 
 
 def parse_hashlib_blacklist(envstr):
-    """Helper to load CONFIG['HASHLIB_BLACKLIST']"""
-    return set(map(lambda s: s.strip().lower(), envstr.split(',')))
+    """Parse a comma separated string list of strings to a python set of strings."""
+    return set(map(lambda s: s.strip().lower(), envstr.split(",")))
 
 
 CONFIG = {
-    'DEBUG': os.environ.get('DEBUG', '') in [1, '1', 'True', 'true'],
-    'SECRET': os.environ.get('SECRET'),
-    'S3_REGION': os.environ.get('S3_REGION', 'eu-west-1'),
-    'HASHLIB_BLACKLIST': parse_hashlib_blacklist(os.getenv('HASHLIB_BLACKLIST', DEFAULT_HASHLIB_BLACKLIST)),
+    "DEBUG": os.environ.get("DEBUG", "") in [1, "1", "True", "true"],
+    "SECRET": os.environ.get("SECRET"),
+    "S3_REGION": os.environ.get("S3_REGION", "eu-west-1"),
+    "HASHLIB_BLACKLIST": parse_hashlib_blacklist(
+        os.getenv("HASHLIB_BLACKLIST", DEFAULT_HASHLIB_BLACKLIST)
+    ),
 }
 
-app = Chalice(app_name='github-webhooks')
-app.debug = CONFIG['DEBUG']
+app = Chalice(app_name="github-webhooks")
+app.debug = CONFIG["DEBUG"]
 
-SNS = boto3.client('sns', region_name=CONFIG['S3_REGION'])
+SNS = boto3.client("sns", region_name=CONFIG["S3_REGION"])
 
 
 def validate_signature(request):
     """Validate that the signature in the header matches the payload."""
-    if CONFIG['SECRET'] is None:
+    if CONFIG["SECRET"] is None:
         return
     try:
-        signature = request.headers['X-Hub-Signature']
-        hashname, hashval = signature.split('=')
+        signature = request.headers["X-Hub-Signature"]
+        hashname, hashval = signature.split("=")
     except (KeyError, ValueError):
         raise BadRequestError()
 
-    if (hashname in CONFIG['HASHLIB_BLACKLIST']) or \
-            (hashname not in hashlib.algorithms_available):
-        raise BadRequestError('X-Hub-Signature hash algorithm unavailable')
+    if (hashname in CONFIG["HASHLIB_BLACKLIST"]) or (
+        hashname not in hashlib.algorithms_available
+    ):
+        raise BadRequestError("X-Hub-Signature hash algorithm unavailable")
 
-    digest = hmac.new(CONFIG['SECRET'].encode(), request.raw_body.encode(), hashname) \
-        .hexdigest()
-    if not hmac.compare_digest(digest.encode(), hashval.encode('utf-8')):
-        raise UnauthorizedError('X-Hub-Signature mismatch')
+    digest = hmac.new(
+        CONFIG["SECRET"].encode(), request.raw_body.encode(), hashname
+    ).hexdigest()
+    if not hmac.compare_digest(digest.encode(), hashval.encode("utf-8")):
+        raise UnauthorizedError("X-Hub-Signature mismatch")
 
 
-@app.route('/{integration}', methods=['POST'])
+@app.route("/{integration}", methods=["POST"])
 def index(integration):
     """Consume GitHub webhook and publish hooks to AWS SNS."""
     request = app.current_request
     validate_signature(request)
 
     try:
-        event = request.headers['X-GitHub-Event']
+        event = request.headers["X-GitHub-Event"]
     except KeyError:
         raise BadRequestError()
 
-    sns_topics = SNS.list_topics()['Topics']
-    topic_arns = {
-        t['TopicArn'].rsplit(':')[-1]: t['TopicArn']
-        for t in sns_topics
-        }
-    topic = f'{integration}_{event}'
+    sns_topics = SNS.list_topics()["Topics"]
+    topic_arns = {t["TopicArn"].rsplit(":")[-1]: t["TopicArn"] for t in sns_topics}
+    topic = f"{integration}_{event}"
     if topic not in topic_arns.keys():
-        topic_arns[topic] = SNS.create_topic(Name=topic)['TopicArn']
+        topic_arns[topic] = SNS.create_topic(Name=topic)["TopicArn"]
 
     SNS.publish(
         TargetArn=topic_arns[topic],
         Subject=event,
-        Message=json.dumps({'default': json.dumps(request.json_body)}),
-        MessageStructure='json'
+        Message=json.dumps({"default": json.dumps(request.json_body)}),
+        MessageStructure="json",
     )
 
-    return {'Code': 'Ok', 'Message': 'Webhook received.'}
+    return {"Code": "Ok", "Message": "Webhook received."}
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,15 +3,20 @@ import os
 import uuid
 
 import pytest
+from app import (
+    CONFIG,
+    DEFAULT_HASHLIB_BLACKLIST,
+    SNS,
+    app,
+    index,
+    parse_hashlib_blacklist,
+)
 from botocore.stub import Stubber
-from chalice import BadRequestError
-from chalice import UnauthorizedError
-
-from app import app, index, CONFIG, SNS, parse_hashlib_blacklist, DEFAULT_HASHLIB_BLACKLIST
+from chalice import BadRequestError, UnauthorizedError
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(TEST_DIR, 'push.json')) as fp:
+with open(os.path.join(TEST_DIR, "push.json")) as fp:
     push = json.load(fp)
 
 
@@ -23,109 +28,131 @@ class Request(object):
 
 
 class TestApp(object):
-
     @pytest.fixture(autouse=True)
     def _set_up(self, monkeypatch):
-        CONFIG['SECRET'] = None
+        CONFIG["SECRET"] = None
 
     def test_blacklist_default(self):
-        assert parse_hashlib_blacklist(DEFAULT_HASHLIB_BLACKLIST) \
-               == set(['crc32', 'crc32c', 'md4', 'md5', 'mdc2'])
+        assert parse_hashlib_blacklist(DEFAULT_HASHLIB_BLACKLIST) == set(
+            ["crc32", "crc32c", "md4", "md5", "mdc2"]
+        )
 
     def test_blacklist_custom(self):
-        assert parse_hashlib_blacklist('SHA1,POLY1305-HMAC,INVENTED') \
-               == set(['invented', 'sha1', 'poly1305-hmac'])
+        assert parse_hashlib_blacklist("SHA1,POLY1305-HMAC,INVENTED") == set(
+            ["invented", "sha1", "poly1305-hmac"]
+        )
 
     def test_no_secret(self):
-        app.current_request = Request(push, {
-            'X-GitHub-Event': 'push',
-            'X-Hub-Signature': 'whatever',
-            'X-GitHub-Delivery': uuid.uuid4().hex,
-        })
+        app.current_request = Request(
+            push,
+            {
+                "X-GitHub-Event": "push",
+                "X-Hub-Signature": "whatever",
+                "X-GitHub-Delivery": uuid.uuid4().hex,
+            },
+        )
 
         stubber = Stubber(SNS)
-        stubber.add_response('list_topics', {'Topics': [{'TopicArn': 'arn:foo:bar:123_push'}]})
-        stubber.add_response('publish', {'MessageId': '1234'})
+        stubber.add_response(
+            "list_topics", {"Topics": [{"TopicArn": "arn:foo:bar:123_push"}]}
+        )
+        stubber.add_response("publish", {"MessageId": "1234"})
         with stubber:
-            response = index('123')
-        assert response == {'Code': 'Ok', 'Message': 'Webhook received.'}
+            response = index("123")
+        assert response == {"Code": "Ok", "Message": "Webhook received."}
 
     def test_no_signature(self):
-        CONFIG['SECRET'] = 'very-secret'
-        app.current_request = Request(push, {
-            'X-GitHub-Event': 'push',
-            'X-GitHub-Delivery': uuid.uuid4().hex,
-        })
+        CONFIG["SECRET"] = "very-secret"
+        app.current_request = Request(
+            push, {"X-GitHub-Event": "push", "X-GitHub-Delivery": uuid.uuid4().hex}
+        )
 
         with pytest.raises(BadRequestError):
-            index('123')
+            index("123")
 
     def test_wrong_signature_format(self):
-        CONFIG['SECRET'] = 'very-secret'
-        app.current_request = Request(push, {
-            'X-GitHub-Event': 'push',
-            'X-Hub-Signature': 'whatever',
-            'X-GitHub-Delivery': uuid.uuid4().hex,
-        })
+        CONFIG["SECRET"] = "very-secret"
+        app.current_request = Request(
+            push,
+            {
+                "X-GitHub-Event": "push",
+                "X-Hub-Signature": "whatever",
+                "X-GitHub-Delivery": uuid.uuid4().hex,
+            },
+        )
 
         with pytest.raises(BadRequestError):
-            index('123')
+            index("123")
 
     def test_blacklist_checksum_default(self):
-        CONFIG['SECRET'] = 'very-secret'
-        app.current_request = Request(push, {
-            'X-GitHub-Event': 'push',
-            'X-Hub-Signature': 'crc32=whatever',
-            'X-GitHub-Delivery': uuid.uuid4().hex,
-        })
+        CONFIG["SECRET"] = "very-secret"
+        app.current_request = Request(
+            push,
+            {
+                "X-GitHub-Event": "push",
+                "X-Hub-Signature": "crc32=whatever",
+                "X-GitHub-Delivery": uuid.uuid4().hex,
+            },
+        )
 
         with pytest.raises(BadRequestError):
-            index('123')
+            index("123")
 
     def test_blacklist_checksum_custom(self):
-        CONFIG['SECRET'] = 'very-secret'
-        CONFIG['HASHLIB_BLACKLIST'] = set(['poly1305-hmac'])
-        app.current_request = Request(push, {
-            'X-GitHub-Event': 'push',
-            'X-Hub-Signature': 'poly1305-hmac=whatever',
-            'X-GitHub-Delivery': uuid.uuid4().hex,
-        })
+        CONFIG["SECRET"] = "very-secret"
+        CONFIG["HASHLIB_BLACKLIST"] = set(["poly1305-hmac"])
+        app.current_request = Request(
+            push,
+            {
+                "X-GitHub-Event": "push",
+                "X-Hub-Signature": "poly1305-hmac=whatever",
+                "X-GitHub-Delivery": uuid.uuid4().hex,
+            },
+        )
 
         with pytest.raises(BadRequestError):
-            index('123')
+            index("123")
 
     def test_wrong_signature(self):
-        CONFIG['SECRET'] = 'very-secret'
-        app.current_request = Request(push, {
-            'X-GitHub-Event': 'push',
-            'X-Hub-Signature': 'sha1=whatever',
-            'X-GitHub-Delivery': uuid.uuid4().hex,
-        })
+        CONFIG["SECRET"] = "very-secret"
+        app.current_request = Request(
+            push,
+            {
+                "X-GitHub-Event": "push",
+                "X-Hub-Signature": "sha1=whatever",
+                "X-GitHub-Delivery": uuid.uuid4().hex,
+            },
+        )
 
         with pytest.raises(UnauthorizedError):
-            index('123')
+            index("123")
 
     def test_missing_event_type(self):
-        app.current_request = Request(push, {
-            'X-Hub-Signature': 'sha=whatever',
-            'X-GitHub-Delivery': uuid.uuid4().hex,
-        })
+        app.current_request = Request(
+            push,
+            {"X-Hub-Signature": "sha=whatever", "X-GitHub-Delivery": uuid.uuid4().hex},
+        )
 
         with pytest.raises(BadRequestError):
-            index('123')
+            index("123")
 
     def test_create_missing_topic(self):
-        app.current_request = Request(push, {
-            'X-GitHub-Event': 'comment',
-            'X-Hub-Signature': 'sha=whatever',
-            'X-GitHub-Delivery': uuid.uuid4().hex,
-        })
+        app.current_request = Request(
+            push,
+            {
+                "X-GitHub-Event": "comment",
+                "X-Hub-Signature": "sha=whatever",
+                "X-GitHub-Delivery": uuid.uuid4().hex,
+            },
+        )
 
         stubber = Stubber(SNS)
-        stubber.add_response('list_topics', {'Topics': [{'TopicArn': 'arn:foo:bar:push'}]})
-        stubber.add_response('create_topic', {'TopicArn': 'arn:foo:baz:123_push'})
-        stubber.add_response('publish', {'MessageId': '1234'})
+        stubber.add_response(
+            "list_topics", {"Topics": [{"TopicArn": "arn:foo:bar:push"}]}
+        )
+        stubber.add_response("create_topic", {"TopicArn": "arn:foo:baz:123_push"})
+        stubber.add_response("publish", {"MessageId": "1234"})
 
         with stubber:
-            response = index('123')
-        assert response == {'Code': 'Ok', 'Message': 'Webhook received.'}
+            response = index("123")
+        assert response == {"Code": "Ok", "Message": "Webhook received."}
